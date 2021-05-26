62ad0f1acba01810e18edad09c36a52f9ca2d708
Ondrej Sika <ondrej@ondrejsika.com>
2021-05-26 19:56:11 +0200

    feat: Add GoRawSendMessage to support custom headers, v0.3.0

--- lib/gosendmail.go
+++ lib/gosendmail.go
@@ -8,22 +8,26 @@ package lib
 
 import "net/smtp"
 
-// GoSendMail send an email from Go using SMTP
-func GoSendMail(smtpHost string, smtpPort string, from string, password string, to string, subject string, message string) error {
-	emailMessage := []byte("To: " + to + "\r\n" +
-		"From: " + from + "\r\n" +
-		"Subject: " + subject + "\r\n" +
-		"\r\n" +
-		message + "\r\n")
-
+// GoRawSendMail send a raw from Go using SMTP
+func GoRawSendMail(smtpHost string, smtpPort string, from string, password string, to string, rawMessage []byte) error {
 	var auth smtp.Auth
 	if password != "" {
 		auth = smtp.PlainAuth(from, from, password, smtpHost)
 	}
 
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, emailMessage)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, rawMessage)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// GoSendMail send an email from Go using SMTP
+func GoSendMail(smtpHost string, smtpPort string, from string, password string, to string, subject string, message string) error {
+	rawMessage := []byte("To: " + to + "\r\n" +
+		"From: " + from + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"\r\n" +
+		message + "\r\n")
+	return GoRawSendMail(smtpHost, smtpPort, from, password, to, rawMessage)
 }
