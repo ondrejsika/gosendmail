663e668ca8805b2fa3c4df0e038f6dc1cc3837ec
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-05 07:39:40 +0100

    feat!: Add user param to have different user and from (for AWS SES)

--- lib/gosendmail.go
+++ lib/gosendmail.go
@@ -9,10 +9,10 @@ package lib
 import "net/smtp"
 
 // GoRawSendMail send a raw from Go using SMTP
-func GoRawSendMail(smtpHost string, smtpPort string, from string, password string, to string, rawMessage []byte) error {
+func GoRawSendMail(smtpHost string, smtpPort string, user string, password string, from string, to string, rawMessage []byte) error {
 	var auth smtp.Auth
 	if password != "" {
-		auth = smtp.PlainAuth(from, from, password, smtpHost)
+		auth = smtp.PlainAuth(from, user, password, smtpHost)
 	}
 
 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, rawMessage)
@@ -23,11 +23,11 @@ func GoRawSendMail(smtpHost string, smtpPort string, from string, password strin
 }
 
 // GoSendMail send an email from Go using SMTP
-func GoSendMail(smtpHost string, smtpPort string, from string, password string, to string, subject string, message string) error {
+func GoSendMail(smtpHost string, smtpPort string, user string, password string, from string, to string, subject string, message string) error {
 	rawMessage := []byte("To: " + to + "\r\n" +
 		"From: " + from + "\r\n" +
 		"Subject: " + subject + "\r\n" +
 		"\r\n" +
 		message + "\r\n")
-	return GoRawSendMail(smtpHost, smtpPort, from, password, to, rawMessage)
+	return GoRawSendMail(smtpHost, smtpPort, user, password, from, to, rawMessage)
 }
